066927108b2017aefb797ef09972c492a485003e
lamine785260388 <lamine785260388@gmail.com>
2025-02-14 21:16:39 +0000

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SiteVitrineAdamaLassana/',  // 🔥 Important pour GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets', // 🔥 Garde les fichiers organisés
-  },
-  server: {
-    open: true,
-  },
 })
